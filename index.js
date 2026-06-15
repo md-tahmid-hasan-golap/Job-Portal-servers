386a8eb16fca3409a57be2eb90794924738ec44c
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-06-15 16:20:50 +0600

    resolve

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 
 
-const uri = process.env.DB_PASS;
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
